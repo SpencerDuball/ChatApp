f03a1384b3da33a507d6eaa5aee9cee56efa0d6b
Spencer Duball <spencerduball@gmail.com>
2020-11-24 17:21:20 -0600

    Working on authHooks.ts will transition to using aws-amplify for auth

--- frontend/src/context/auth/AuthContext.tsx
+++ frontend/src/context/auth/AuthContext.tsx
@@ -1,14 +1,41 @@
 import * as React from "react";
-import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
+import { useInitiateAuth } from "./authHooks";
 
+// define the context
 interface IAuthContext {}
-
 const initialAuthContext = {};
+
+//////////////////////////////////////////////////////////////////////////////
+// Create the actions for the AuthContextReducer
+export interface IAuthContextAction {
+  type: string;
+  payload: any;
+}
+//////////////////////////////////////////////////////////////////////////////
+
+// create the reducer
+const authContextReducer = (
+  state: IAuthContext,
+  action: IAuthContextAction
+) => {
+  switch (action.type) {
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+      return state;
+    }
+  }
+};
 
 export const AuthContext = React.createContext<IAuthContext>(
   initialAuthContext
 );
 
 export const AuthContextProvider = (props: { children: React.ReactNode }) => {
+  const [state, dispatch] = React.useReducer(
+    authContextReducer,
+    initialAuthContext
+  );
+  useInitiateAuth(dispatch);
+
   return <AuthContext.Provider value={{}} {...props} />;
 };
