53cb7a9d7cbf4af804f06d775f5cb42078fea328
Spencer Duball <slduball@Spencers-MacBook-Pro.local>
2020-11-24 21:31:26 -0600

    Added @chakra-ui & started working on background graphics

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+import BackgroundShapes from "../components/svg/BackgroundShapes";
 
 const Index = () => {
   return (
-    <div>
+    <Box width="100%" height="100vh" position="relative">
+      <BackgroundShapes
+        position="fixed"
+        height="100%"
+        color="#FBF6E3"
+        marginLeft="50%"
+        transform="translateX(-50%)"
+      />
       <Head>
         <title>ChatApp</title>
       </Head>
-      <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>
-    </div>
+    </Box>
   );
 };
 
