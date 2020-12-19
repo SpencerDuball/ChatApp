2f7d318e43e8b4547b5156cf7e62523f0fd177ac
Spencer Duball <spencerduball@gmail.com>
2020-12-19 15:08:15 -0600

    Setup barebones NextJS app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,27 +1,5 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import theme from "../theme/theme";
-import Amplify from "aws-amplify";
-import awsConfig from "../aws-config";
-import { AppContextProvider } from "context/app-context/AppContext";
-import Head from "next/head";
-
-Amplify.configure(awsConfig);
-
-const extendedTheme = extendTheme(theme);
-
 const _app = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <title>ChatApp</title>
-      </Head>
-      <AppContextProvider>
-        <ChakraProvider theme={extendedTheme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </AppContextProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default _app;
