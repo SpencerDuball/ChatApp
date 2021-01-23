f3c632914a8ebbb7007917fab37c6ee3edd04e93
Spencer Duball <spencerduball@gmail.com>
2021-01-22 21:17:58 -0600

    Rewrote AuthStack

--- backend/bin/chatapp.ts
+++ backend/bin/chatapp.ts
@@ -2,26 +2,12 @@
 import "source-map-support/register";
 import * as dotenv from "dotenv";
 import * as cdk from "@aws-cdk/core";
-import { DbStack } from "../lib/db-stack/DbStack";
-import { ComputeStack } from "../lib/compute-stack/ComputeStack";
-import { AuthStack } from "../lib/auth-stack/AuthStack";
 
 // configure environment variables
 dotenv.config();
 
+// create the application
 const app = new cdk.App();
-const dbStack = new DbStack(app, "DbStack");
-const computeStack = new ComputeStack(app, "ContactsServiceStack", {
-  ddbTable: dbStack.dynamoDbTable,
-});
-new AuthStack(app, "AuthStack", {
-  contactsApi: computeStack.apiPlane.httpApi,
-  wsApi: computeStack.apiPlane.wsApi,
-});
 
 // add 'Project' tag
-cdk.Tags.of(app).add("Project", "ChatApp", {
-  // prevent from tagging Api Gateway V2. This prevents an error where using the openapi body
-  // attribute of the cloudformation resource does not also allow tags to be applied
-  excludeResourceTypes: ["AWS::ApiGatewayV2::Api"],
-});
+cdk.Tags.of(app).add("Project", "ChatApp");
--- backend/lib/auth-stack/AuthStack.ts
+++ backend/lib/auth-stack/AuthStack.ts
@@ -1,183 +1,228 @@
 import * as cdk from "@aws-cdk/core";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as iam from "@aws-cdk/aws-iam";
-import * as apiGwV2 from "@aws-cdk/aws-apigatewayv2";
-import * as ssm from "@aws-cdk/aws-ssm";
+
+interface AuthStackPropsI extends cdk.StackProps {}
+
+/**
+ * Creates a CfnUserPool construct.
+ *
+ * @param scope - scope in which this resource is defined.
+ * @param id - scoped id of the resouce.
+ */
+const createUserPool = (scope: cdk.Stack, id: string) =>
+  new cognito.CfnUserPool(scope, id, {
+    accountRecoverySetting: {
+      recoveryMechanisms: [{ name: "verified_email", priority: 1 }],
+    },
+    autoVerifiedAttributes: ["email"],
+    emailConfiguration: {
+      emailSendingAccount: "COGNITO_DEFAULT",
+    },
+    mfaConfiguration: "OFF",
+    policies: {
+      passwordPolicy: {
+        minimumLength: 8,
+        requireLowercase: false,
+        requireNumbers: false,
+        requireSymbols: false,
+        requireUppercase: false,
+        temporaryPasswordValidityDays: 7,
+      },
+    },
+    usernameAttributes: ["email"],
+    usernameConfiguration: { caseSensitive: false },
+    verificationMessageTemplate: {
+      defaultEmailOption: "CONFIRM_WITH_CODE",
+    },
+  });
+
+/**
+ * Creates a CfnUserPoolClient construct configured for web.
+ *
+ * @param scope - scope in which this resource is defined.
+ * @param id - scoped id of the resouce.
+ * @param props - the UserPool that this UserPoolClient is attached to.
+ */
+const createWebClient = (
+  scope: cdk.Stack,
+  id: string,
+  props: { userPool: cognito.CfnUserPool }
+) =>
+  new cognito.CfnUserPoolClient(scope, id, {
+    accessTokenValidity: 1, // in hours
+    explicitAuthFlows: [
+      "ALLOW_USER_SRP_AUTH",
+      "ALLOW_REFRESH_TOKEN_AUTH",
+      "ALLOW_CUSTOM_AUTH",
+      "ALLOW_USER_PASSWORD_AUTH",
+    ],
+    generateSecret: false,
+    idTokenValidity: 1, // in hours
+    preventUserExistenceErrors: "ENABLED",
+    refreshTokenValidity: 30, // in days
+    supportedIdentityProviders: ["COGNITO"],
+    userPoolId: props.userPool.ref,
+  });
+
+/**
+ * Creates a CfnIdentityPool construct.
+ *
+ * @param scope - scope in which this resource is defined.
+ * @param id - scoped id of the resouce.
+ * @param props - The properties used to create a CfnIdentityPool.
+ */
+const createIdentityPool = (
+  scope: cdk.Stack,
+  id: string,
+  props: {
+    userPool: cognito.CfnUserPool;
+    userPoolClients: { web: cognito.CfnUserPoolClient };
+  }
+) =>
+  new cognito.CfnIdentityPool(scope, id, {
+    allowClassicFlow: false,
+    allowUnauthenticatedIdentities: false,
+    cognitoIdentityProviders: [
+      {
+        clientId: props.userPoolClients.web.ref,
+        providerName: props.userPool.attrProviderName,
+        serverSideTokenCheck: true,
+      },
+    ],
+  });
+
+/**
+ * Create the IAM role for the authenticated users.
+ *
+ * @param scope - scope in which this resource is defined.
+ * @param id - scoped id of the resource.
+ * @param props - The properties used to create a CfnIdentityPool.
+ */
+const createAuthenticatedRoles = (
+  scope: cdk.Stack,
+  id: string,
+  props: { identityPool: cognito.CfnIdentityPool }
+) =>
+  new iam.CfnRole(scope, id, {
+    assumeRolePolicyDocument: iam.PolicyDocument.fromJson({
+      Statement: [
+        {
+          Effect: "Allow",
+          Principal: { Federated: "cognito-identity.amazonaws.com" },
+          Action: "sts:AssumeRoleWithWebIdentity",
+          Condition: {
+            StringEquals: {
+              "cognito-identity.amazonaws.com:aud": props.identityPool.ref,
+            },
+            "ForAnyValue:StringLike": {
+              "cognito-identity.amazonaws.com:amr": "authenticated",
+            },
+          },
+        },
+      ],
+    }),
+    policies: [
+      {
+        policyDocument: iam.PolicyDocument.fromJson({
+          Statement: [
+            {
+              Effect: "Allow",
+              Action: ["execute-api:*"],
+              Resource: [
+                // http api
+                `arn:aws:execute-api:${scope.region}:${scope.account}:*/*/*/contact`,
+                `arn:aws:execute-api:${scope.region}:${scope.account}:*/*/*/contact/*`,
+                `arn:aws:execute-api:${scope.region}:${scope.account}:*/*/*/contacts`,
+                // ws api
+                `arn:aws:execute-api:${scope.region}:${scope.account}:*/*`,
+              ],
+            },
+          ],
+        }),
+        policyName: "ChatAppContactsApiInvokePolicy",
+      },
+    ],
+  });
+
+/**
+ * Creates an IdentityPoolRoleAttachemnt.
+ *
+ * @param scope - scope in which this resource is defined.
+ * @param id - scoped id of the resource.
+ * @param props - The properties used to create a CfnIdentityPool.
+ */
+const createIdentityPoolRoleAttachment = (
+  scope: cdk.Stack,
+  id: string,
+  props: {
+    identityPool: cognito.CfnIdentityPool;
+    userPool: cognito.CfnUserPool;
+    webClient: cognito.CfnUserPoolClient;
+    authenticatedRole: iam.CfnRole;
+  }
+) =>
+  new cognito.CfnIdentityPoolRoleAttachment(scope, id, {
+    identityPoolId: props.identityPool.ref,
+    roleMappings: {
+      ChatAppTesterShizip: {
+        ambiguousRoleResolution: "AuthenticatedRole",
+        identityProvider: `cognito-idp.${scope.region}.amazonaws.com/${props.userPool.ref}:${props.webClient.ref}`,
+        type: "Token",
+      },
+    },
+    roles: {
+      authenticated: props.authenticatedRole.attrArn,
+    },
+  });
 
 export class AuthStack extends cdk.Stack {
   private userPool: cognito.CfnUserPool;
-  private userPoolClient: cognito.CfnUserPoolClient;
+  private userPoolClients: {
+    web: cognito.CfnUserPoolClient;
+  };
   private identityPool: cognito.CfnIdentityPool;
+  private authenticatedRole: iam.CfnRole;
+  private identityPoolRoleAttachment: cognito.CfnIdentityPoolRoleAttachment;
 
-  constructor(
-    scope: cdk.Construct,
-    id: string,
-    props: cdk.StackProps & {
-      contactsApi: apiGwV2.CfnApi;
-      wsApi: apiGwV2.CfnApi;
-    }
-  ) {
+  constructor(scope: cdk.Construct, id: string, props: AuthStackPropsI) {
     super(scope, id, props);
 
-    this.userPool = new cognito.CfnUserPool(this, "ChatAppUserPool", {
-      accountRecoverySetting: {
-        recoveryMechanisms: [{ name: "verified_email", priority: 1 }],
-      },
-      autoVerifiedAttributes: ["email"],
-      emailConfiguration: {
-        emailSendingAccount: "COGNITO_DEFAULT",
-      },
-      mfaConfiguration: "OFF",
-      policies: {
-        passwordPolicy: {
-          minimumLength: 8,
-          requireLowercase: false,
-          requireNumbers: false,
-          requireSymbols: false,
-          requireUppercase: false,
-          temporaryPasswordValidityDays: 7,
-        },
-      },
-      usernameAttributes: ["email"],
-      usernameConfiguration: { caseSensitive: false },
-      verificationMessageTemplate: {
-        defaultEmailOption: "CONFIRM_WITH_CODE",
-      },
-    });
+    // create the UserPool
+    this.userPool = createUserPool(this, "ChatAppUserPool");
 
-    this.userPoolClient = new cognito.CfnUserPoolClient(
-      this,
-      "ChatAppUserPoolClient",
-      {
-        accessTokenValidity: 1, // in hours
-        explicitAuthFlows: [
-          "ALLOW_USER_SRP_AUTH",
-          "ALLOW_REFRESH_TOKEN_AUTH",
-          "ALLOW_CUSTOM_AUTH",
-          "ALLOW_USER_PASSWORD_AUTH",
-        ],
-        generateSecret: false,
-        idTokenValidity: 1, // in hours
-        preventUserExistenceErrors: "ENABLED",
-        refreshTokenValidity: 30, // in days
-        supportedIdentityProviders: ["COGNITO"],
-        userPoolId: this.userPool.ref,
-      }
-    );
-
-    this.identityPool = new cognito.CfnIdentityPool(
-      this,
-      "ChatAppIdentityPool",
-      {
-        allowClassicFlow: false,
-        allowUnauthenticatedIdentities: false,
-        cognitoIdentityProviders: [
-          {
-            clientId: this.userPoolClient.ref,
-            providerName: this.userPool.attrProviderName,
-            serverSideTokenCheck: true,
-          },
-        ],
-      }
-    );
-
-    const authenticatedRole = new iam.CfnRole(this, "ChatAppDefaultAuthRole", {
-      assumeRolePolicyDocument: iam.PolicyDocument.fromJson({
-        Statement: [
-          {
-            Effect: "Allow",
-            Principal: { Federated: "cognito-identity.amazonaws.com" },
-            Action: "sts:AssumeRoleWithWebIdentity",
-            Condition: {
-              StringEquals: {
-                "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
-              },
-              "ForAnyValue:StringLike": {
-                "cognito-identity.amazonaws.com:amr": "authenticated",
-              },
-            },
-          },
-        ],
+    // create the UserPoolClients
+    this.userPoolClients = {
+      web: createWebClient(this, "ChatAppUserPoolClient-Web", {
+        userPool: this.userPool,
       }),
-      policies: [
-        {
-          policyDocument: iam.PolicyDocument.fromJson({
-            Statement: [
-              {
-                Effect: "Allow",
-                Action: ["execute-api:*"],
-                Resource: [
-                  // httpapi
-                  `arn:aws:execute-api:${this.region}:${this.account}:${props.contactsApi.ref}/*/*/contact`,
-                  `arn:aws:execute-api:${this.region}:${this.account}:${props.contactsApi.ref}/*/*/contact/*`,
-                  `arn:aws:execute-api:${this.region}:${this.account}:${props.contactsApi.ref}/*/*/contacts`,
-                  // ws api
-                  `arn:aws:execute-api:${this.region}:${this.account}:${props.wsApi.ref}/*`,
-                ],
-              },
-            ],
-          }),
-          policyName: "ChatAppContactsApiInvokePolicy",
-        },
-      ],
+    };
+
+    // create the IdentityPool
+    this.identityPool = createIdentityPool(this, "ChatAppIdentityPool", {
+      userPool: this.userPool,
+      userPoolClients: { web: this.userPoolClients.web },
     });
 
-    const identityPoolAuthRoleAttachment = new cognito.CfnIdentityPoolRoleAttachment(
+    // create the authenticated role for IdentityPool users
+    this.authenticatedRole = createAuthenticatedRoles(
       this,
-      "IdentityPoolAuthRoleAttachment",
+      "ChatAppAuthenticatedRole",
       {
-        identityPoolId: this.identityPool.ref,
-        roleMappings: {
-          ChatAppTesterShizip: {
-            ambiguousRoleResolution: "AuthenticatedRole",
-            identityProvider: `cognito-idp.${this.region}.amazonaws.com/${this.userPool.ref}:${this.userPoolClient.ref}`,
-            type: "Token",
-          },
-        },
-        roles: {
-          authenticated: authenticatedRole.attrArn,
-        },
+        identityPool: this.identityPool,
       }
     );
 
-    // create parameters
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-Region", {
-      description: "The aws region.",
-      name: "/ChatApp/test/region",
-      type: "String",
-      value: this.region,
-    });
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-UserPoolClientId", {
-      description: "The UserPoolClientId from the Cognito user pool.",
-      name: "/ChatApp/test/user_pool_client_id",
-      type: "String",
-      value: this.userPoolClient.ref,
-    });
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-UserPoolId", {
-      description: "The UserPoolId from the Cognito user pool.",
-      name: "/ChatApp/test/user_pool_id",
-      type: "String",
-      value: this.userPool.ref,
-    });
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-IdentityPoolId", {
-      description: "The IdentityPoolId from the Cognito user pool",
-      name: "/ChatApp/test/identity_pool_id",
-      type: "String",
-      value: this.identityPool.ref,
-    });
-    const USERNAME = "spencerduball@gmail.com";
-    const PASSWORD = "password";
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-TestCognitoUsername", {
-      description: "The username of the test cognito user.",
-      name: "/ChatApp/test/username",
-      type: "String",
-      value: USERNAME,
-    });
-    new ssm.CfnParameter(this, "ChatAppSSM-Test-TestCognitoPassword", {
-      description: "The password of the test cognito user.",
-      name: "/ChatApp/test/password",
-      type: "String",
-      value: PASSWORD,
-    });
+    // create the IdentityPoolRoleAttachement
+    this.identityPoolRoleAttachment = createIdentityPoolRoleAttachment(
+      this,
+      "ChatAppIdentityPoolRoleAttachement",
+      {
+        identityPool: this.identityPool,
+        userPool: this.userPool,
+        webClient: this.userPoolClients.web,
+        authenticatedRole: this.authenticatedRole,
+      }
+    );
   }
 }
